16a3b869d2161fc904717cec69c70f51e08c820c
Benjamin Wöster <benjamin.woester@gmail.com>
2014-09-18 00:15:23 +0200

    fix: remove default_schema from sql script

--- architecture/iw-140917_213245_add_external_identities.sql
+++ architecture/iw-140917_213245_add_external_identities.sql
@@ -2,7 +2,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-ALTER TABLE `default_schema`.`user` 
+ALTER TABLE `user` 
 DROP COLUMN `status`,
 DROP COLUMN `role`,
 DROP COLUMN `email`,
@@ -13,7 +13,7 @@ DROP COLUMN `username`,
 ADD COLUMN `name` VARCHAR(255) NOT NULL AFTER `id`,
 ADD UNIQUE INDEX `name_UNIQUE` (`name` ASC);
 
-CREATE TABLE IF NOT EXISTS `default_schema`.`identity` (
+CREATE TABLE IF NOT EXISTS `identity` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `internal_user_id` INT(11) UNSIGNED NOT NULL,
   `auth_provider` VARCHAR(32) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `default_schema`.`identity` (
   INDEX `fk_identity_user1_idx` (`internal_user_id` ASC),
   CONSTRAINT `fk_identity_user1`
     FOREIGN KEY (`internal_user_id`)
-    REFERENCES `default_schema`.`user` (`id`)
+    REFERENCES `user` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
