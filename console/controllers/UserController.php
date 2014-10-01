dd467a43e20193ee40c20711ca0fa0b6030fbc7f
Benjamin Wöster <benjamin.woester@gmail.com>
2014-10-01 17:52:03 +0200

    add: console command to create users

---
+++ console/controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace console\controllers;
+
+use yii\console\Controller;
+use common\models\User;
+
+/**
+ * Description of UserController
+ *
+ * @author ben
+ */
+class UserController extends Controller
+{
+  public function __construct($id, $module, $config = array())
+  {
+    $this->defaultAction = 'create';
+    parent::__construct($id, $module, $config);
+  }
+
+  public function actionCreate( $userName )
+  {
+    $user = new User();
+    $user->name = $userName;
+    
+    if (!$user->save())
+    {
+      foreach ($user->getFirstErrors() as $error) {
+        echo $error . "\n";
+      }
+    }
+  }
+}
