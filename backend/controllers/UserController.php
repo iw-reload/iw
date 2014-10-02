80d537928bb801f16b6ae36e80ab677dd1d029f5
Benjamin Wöster <benjamin.woester@gmail.com>
2014-10-02 14:22:53 +0200

    fix: access control declaration

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -22,6 +22,7 @@ class UserController extends Controller
       'access' => [
         'class' => AccessControl::className(),
         'ruleConfig' => [
+          'class' => 'yii\filters\AccessRule',
           'allow' => true,
         ],
         'rules' => [
