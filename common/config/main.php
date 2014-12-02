99e97e1946af10d1aa4bddc9153c06f589790eb3
Benjamin Wöster <benjamin.woester@gmail.com>
2014-12-02 01:36:39 +0100

    add: configure doctrine component

--- common/config/main.php
+++ common/config/main.php
@@ -8,6 +8,24 @@ return [
     'cache' => [
       'class' => 'yii\caching\FileCache',
     ],
+    'db' => [
+      'class' => 'yii\db\Connection',
+    ],
+    'doctrine' => [
+      'class' => 'common\components\doctrine\DoctrineComponent',
+      'connection' => [
+        'params' => [],
+      ],
+      'entityManager' => [
+        'config' => function() {
+          $paths = [
+            Yii::getAlias('@common/entities'),
+          ];
+          $isDevMode = YII_DEBUG;
+          return \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration( $paths, $isDevMode );
+        },
+      ],
+    ],
     'time' => [
       'class' => 'common\components\TimeComponent',
     ],
